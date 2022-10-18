9c74244d190cd66bf0daf4cdc090246540aa1291
NyXXiR <114169262+NyXXiR@users.noreply.github.com>
2022-10-19 02:39:24 +0900

    221019
    @state
    prop:컴포넌트를 사용하는 외부자 위한 데이터
    state: 컴포넌트 만드는 내부자 위한 데이터

--- VScode/my-react-app/src/App.js
+++ VScode/my-react-app/src/App.js
@@ -1,43 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
-function Header(props){
-  console.log('props',props, props.title)
-  return  <header>
-  <h1><a href="/">{props.title}</a></h1> 
-</header>
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
 }
-function Nav(props){
- const lis =  []
-for(let i=0; i<props.topics.length; i++){
-let t = props.topics[i];
-lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
 }
-return <nav>
-      <ol>
-     {lis}
-      </ol>
-      </nav>
-}
-function Article(props){
-  return <article>
-   <h2>{props.title}</h2>
-    {props.body}
-  </article>
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
 }
 
 function App() {
-  const topics = [
-    {id:1 ,title:'html', body:'html is...'},
-    {id:2 ,title:'css', body:'css is...'},
-    {id:3 ,title:'html', body:'js is...'}
-  ]
-  return (
-   <div>
-  <Header title="WEB"></Header>
-  <Nav topics = {topics}></Nav>
-  <Article title = "Welcome" body = "Hello, Web."></Article>
-  <Article title = "Hi" body = "Hello, React." ></Article>
+  // const _mode = useState("WELCOME");
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
 
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "html", body: "js is..." },
+  ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, Web."></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Article title={title} body={body}></Article>;
+  }
+  return (
+    <div>
+      <Header
+        title="WEB"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
     </div>
   );
 }
