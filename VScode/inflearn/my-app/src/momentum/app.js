20a92d8c79fc57f7b2b97fdb5d0e803b157b99bc
NyXXiR <114169262+NyXXiR@users.noreply.github.com>
2022-10-31 16:46:26 +0900

    .
    .

--- VScode/inflearn/my-app/src/momentum/app.js
+++ VScode/inflearn/my-app/src/momentum/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+const h1 = document.querySelector(".hello h1"); // css 셀렉터를 가져오는 방법
+
+console.log(h1);
+
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+
+  if (currentColor === "blue") {
+    newColor = "tomato";
+    count = 0;
+    console.log(count);
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+h1.addEventListener("click", handleTitleClick);
